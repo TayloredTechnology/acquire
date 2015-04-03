b9a923328d15d59d74e27377b1132f81d40ff176
Eugene Sharygin <eush77@gmail.com>
2015-04-03 11:06:23 +0300

    Add test messages

--- test/test.js
+++ test/test.js
@@ -10,12 +10,14 @@ var path = require('path');
 
 test(function (t) {
   var fixtures = path.join(__dirname, 'fixtures');
-  var basedir = fixtures + '/node_modules/foo/src';
+  var fixture = path.resolve.bind(null, fixtures);
+  var unfixture = path.relative.bind(null, fixtures);
 
+  var basedir = fixture('node_modules/foo/src');
   var paths = {
-    top: path.join(fixtures, 'node_modules/top.json'),
-    middle: path.join(fixtures, 'node_modules/foo/node_modules/middle/index.json'),
-    bottom: path.join(fixtures, 'node_modules/foo/src/node_modules/bottom.json')
+    top: fixture('node_modules/top.json'),
+    middle: fixture('node_modules/foo/node_modules/middle/index.json'),
+    bottom: fixture('node_modules/foo/src/node_modules/bottom.json')
   };
 
   check(basedir, ['bottom']);
@@ -26,14 +28,20 @@ test(function (t) {
 
   function check() {
     var argv = [].slice.call(arguments);
+    var basedir = argv[0];
     var modules = argv.pop();
+
+    var message = function (method) {
+      return [method, 'from', unfixture(basedir),
+              argv.slice(1).map(JSON.stringify)].join(' ');
+    };
 
     t.deepEqual(acquire.resolve.apply(null, argv), zipmap(modules.map(function (m) {
       return [m, paths[m]];
-    })));
+    })), message('acquire.resolve'));
 
     t.deepEqual(acquire.apply(null, argv), zipmap(modules.map(function (m) {
       return [m, m];
-    })));
+    })), message('acquire'));
   }
 });
