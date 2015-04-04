829259503188215982e1a158f0dd819cf28ea018
Eugene Sharygin <eush77@gmail.com>
2015-04-04 14:50:06 +0300

    Skip invalid module names (fix #1)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var findNodeModules = require('find-node-modules'),
     getModuleName = require('filename-to-module-name'),
+    validateNpmPackageName = require('validate-npm-package-name'),
     zipmap = require('zipmap'),
     xtend = require('xtend');
 
@@ -14,6 +15,12 @@ var nodeModulesPaths = function (basedir) {
     cwd: basedir,
     relative: false
   });
+};
+
+
+var validateModuleName = function (name) {
+  var v = validateNpmPackageName(name);
+  return v.validForOldPackages || v.validForNewPackages;
 };
 
 
@@ -44,6 +51,10 @@ acquire.resolve = function (basedir, opts) {
 
   return xtend.apply(null, dirs.map(function (dir) {
     return zipmap(fs.readdirSync(dir).map(function (basename) {
+      if (!validateModuleName(basename)) {
+        return;
+      }
+
       var modulePath = path.resolve(dir, basename);
       var moduleName = getModuleName(modulePath);
       modulePath = (opts.skipFailures ? safeResolve : require.resolve)(modulePath);
